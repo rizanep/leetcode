0cf30d5a6c745146ffcb137e5df44870a4190100
rizanep <khaderrizan@gmail.com>
2025-04-11 10:55:43 +0530

    Time: 4 ms (93.28%), Space: 64.4 MB (55.27%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -6,8 +6,7 @@ var isPalindrome = function(x) {
     var reverse = 0;
     var copy = x;
 
-    //The loop break when the copy of original number becomes zero
-    //Also negative number cannot be a palindrome
+
     while (copy > 0) {
       const digit = copy % 10;
       reverse = reverse * 10 + digit;
