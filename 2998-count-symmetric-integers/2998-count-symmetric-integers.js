4ffaaa49094c557526480ccf16998905082c64b1
rizanep <khaderrizan@gmail.com>
2025-04-16 16:56:17 +0530

    Time: 16 ms (85.15%), Space: 57.9 MB (61.04%) - LeetHub

---
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countSymmetricIntegers = function(low, high) {
+     
+     
+     let result=0
+     for(i=low;i<=high;i++){
+        let s=i.toString()
+        let m=s.length/2
+        if(s.length%2=== 1) continue;
+        let sum1=0
+            for(j=0; j<m;j++){
+                sum1+= s.charCodeAt(j)- s.charCodeAt(j+m)
+            }
+           if(sum1===0){
+                result++
+                
+            }
+        
+     }return result
+};
