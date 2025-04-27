6a0fab489d8d3a45b86ed2bd1ef489e03257764f
rizanep <khaderrizan@gmail.com>
2025-04-27 13:44:27 +0530

    Time: 6 ms (65.74%), Space: 65.5 MB (24.1%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.js
+++ 0326-power-of-three/0326-power-of-three.js
@@ -3,11 +3,8 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    if(n<=0){
-        return false
-    }
-      while(n % 3 === 0){
-        n = n / 3
-    }
-    return n === 1
+      if(n==0) return false;
+    if(n==1) return true;
+    if(n%3!=0)return false;
+    return isPowerOfThree(n/3);
 };
