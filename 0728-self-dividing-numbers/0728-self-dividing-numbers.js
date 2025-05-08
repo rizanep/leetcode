b00090cc7e3937729cd6c37ac32378fc1027da56
rizanep <khaderrizan@gmail.com>
2025-05-08 16:50:25 +0530

    Time: 1 ms (94.74%), Space: 55.6 MB (57.89%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+    const set=new Set()
+    
+    for(i=left;i<=right;i++){
+        let a=i.toString()
+        let k=0
+        for(j=0;j<a.length;j++){
+            if(i%a[j]==0){
+                 k+=1
+            }else{break}
+            if(k==a.length){
+                set.add(i)
+            }
+        }
+    }return Array.from(set)
+};
