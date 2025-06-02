fb0a9a3e63f15039a799862f6f22edb0046a3d61
rizanep <khaderrizan@gmail.com>
2025-06-02 12:46:11 +0530

    Time: 3 ms (4.67%), Space: 59.4 MB (8.5%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if(n<=0)return false
+    if(n===1){return true}
+    
+    
+        return isPowerOfFour(n/4)
+   
+};
