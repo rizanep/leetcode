a08dbd6086d4332a29912baab54039510cdd3292
rizanep <khaderrizan@gmail.com>
2025-06-03 11:16:24 +0530

    Time: 18 ms (25.5%), Space: 57.6 MB (28.82%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    nums.sort((a,b)=> a-b)
+    if(nums[0]!==0){
+        return 0
+    }
+    for(i=0;i<nums.length;i++){
+        
+        if(nums[i+1]!==nums[i]+1){
+            return nums[i]+1
+        }
+    }
+};
