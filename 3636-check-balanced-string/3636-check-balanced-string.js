24e3353d41b68609338bdaf8efdea1181b2dffe8
rizanep <khaderrizan@gmail.com>
2025-06-12 18:04:39 +0530

    Time: 2 ms (38.58%), Space: 55.3 MB (71.16%) - LeetHub

---
+++ 3636-check-balanced-string/3636-check-balanced-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var isBalanced = function(num) {
+    let sum1=0
+    let sum2=0
+    for(i=0;i<num.length;i++){
+        if(i%2==0){
+            sum1+=Number(num[i])
+        }else{sum2+=Number(num[i])}
+    }return sum1==sum2
+};
